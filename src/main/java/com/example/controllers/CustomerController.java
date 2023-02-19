2d94ca876ddd0b572c0b96e2e777d1f393ae679b
Itziar Carrera García <itziar.carrera@hotmail.com>
2023-02-19 17:53:43 +0100

    Feat: create customer-list.html. Fix: update the return path in CustomerController, complete Customer entity.

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -6,12 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
@@ -23,7 +19,7 @@ public class CustomerController {
     public String findAll(Model model) {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
-        return "customer-list";
+        return "customer/customer-list";
     }
 
 
