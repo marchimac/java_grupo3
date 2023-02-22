e728324ef65d4409c156dc567e6f2cd6f0a6d4f6
Itziar Carrera García <itziar.carrera@hotmail.com>
2023-02-22 12:21:03 +0100

    Project - Alan's suggestions

--- src/main/java/com/example/repositories/ProjectRepository.java
+++ src/main/java/com/example/repositories/ProjectRepository.java
@@ -3,5 +3,9 @@ package com.example.repositories;
 import com.example.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    /*List<Project> findAllByCustomerId(Long id); // Listado de proyectos por cliente. Se puede usar en customer-detail.html llamando a esta consulta*/
 }
