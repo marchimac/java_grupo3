7fb70a189d446758a212b1f1e043843636a5c4ed
Itziar Carrera García <itziar.carrera@hotmail.com>
2023-02-22 14:23:11 +0100

    Feat: Employee form & adjustments to make it work.

--- src/main/java/com/example/controllers/EmployeeController.java
+++ src/main/java/com/example/controllers/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.example.controllers;
 
+import com.example.entities.Address;
 import com.example.entities.Employee;
 import com.example.services.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,5 +38,34 @@ public class EmployeeController {
         return "employee/employee-detail";
     }
 
+    @GetMapping("employees/create")
+    public String createForm(Model model) {
+        Employee employee = new Employee();
+        employee.setAddress(new Address()); // Objeto Address dentro de objeto Employee para que se guarde también en BD
+        model.addAttribute("employee", employee);
+        return "employee/employee-form";
+    }
 
+    @GetMapping("employees/{id}/edit")
+    public String editForm(Model model, @PathVariable Long id) {
+        Optional<Employee> employeeOpt = employeeService.findById(id);
+        if (employeeOpt.isPresent())
+            model.addAttribute("employee", employeeOpt.get());
+        else
+            model.addAttribute("error", "Employee not found");
+
+        return "employee/employee-form";
+    }
+
+    @PostMapping("employees")
+    public String saveForm(@ModelAttribute Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees";
+    }
+
+    @GetMapping("employees/{id}/delete")
+    public String deleteById(@PathVariable Long id) {
+        employeeService.deleteById(id);
+        return "redirect:/employees";
+    }
 }
