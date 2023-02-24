bb34c4ff7512c03363bfe3379636b7da43000c54
Beatriz <bitrix24@hotmail.com>
2023-02-24 20:09:56 +0100

    actualizo projectcontroller con getmapping by id

--- src/main/java/com/example/controllers/ProjectController.java
+++ src/main/java/com/example/controllers/ProjectController.java
@@ -7,8 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
@@ -21,4 +23,16 @@ public class ProjectController {
         model.addAttribute("projects", projects);
         return "project-list";
     }
+
+    @GetMapping("projects/{id}")
+    public String findById(Model model, @PathVariable Long id) {
+        Optional<Project> projectOpt = projectService.findById(id);
+        if (projectOpt.isPresent())
+            model.addAttribute("project", projectOpt.get());
+        else
+            model.addAttribute("error", "Project not found");
+        return "project/project-detail";
+    }
+
+
 }
