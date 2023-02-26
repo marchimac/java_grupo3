ff18e790d59d66a6f4d1c5bfc486cb7b20f7cb05
Beatriz <bitrix24@hotmail.com>
2023-02-26 22:08:03 +0100

    added getmapping

--- src/main/java/com/example/controllers/ProjectController.java
+++ src/main/java/com/example/controllers/ProjectController.java
@@ -7,7 +7,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,21 +36,36 @@ public class ProjectController {
         return "project/project-detail";
     }
 
-//    @GetMapping("projects/create")  Not sure about this
-//    public String createForm(Model model) {
-//        Project project = new Project();
-//        project.setCompany(new Company);
-//    }
+    @GetMapping("projects/create")
+    public String createForm(Model model) {
+        model.addAttribute("project", new Project());
+        model.addAttribute("projects", projectService.findAll());
+        return "project/project-form";
+    }
 
-//    @GetMapping("projects/{id}/edit")
-//    public String editForm(Model model, @PathVariable Long id) {
-//        Optional<Project> projectOpt = projectService.findById(id);
-//        if (projectOpt.isPresent())
-//            model.addAttribute("project", projectOpt.get());
-//        else
-//            model.addAttribute("error", "Project not found");
-//        return "project/project-form";
-//    }
+    @GetMapping("projects/{id}/edit")
+    public String editForm(Model model, @PathVariable Long id) {
+        Optional<Project> projectOpt = projectService.findById(id);
+        if (projectOpt.isPresent()) {
+            model.addAttribute("project", projectOpt.get());
+            model.addAttribute("projects", projectService.findAll());
+        } else {
+            model.addAttribute("error", "Project not found");
+        }
 
+        return "project/project-form";
+    }
+
+    @PostMapping("projects")
+    public String saveForm(@ModelAttribute Project project) {
+        projectService.save(project);
+        return "redirect:/projects";
+    }
+
+    @GetMapping("projects/{id}/delete")
+    public String deleteById(@PathVariable Long id) {
+        projectService.deleteById(id);
+        return "redirect:/projects";
+    }
 
 }
